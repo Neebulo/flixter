c3b7e952d1f7df77269f7d4c7f55dcbceca56bfd
neebulo <rhapsodyshadow@yahoo.com>
2017-08-01 07:00:28 +0000

    refactor new and create actions @course | sections controller

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,13 +1,11 @@
 class Instructor::SectionsController < ApplicationController
   def new
-    @course = Course.find(params[:course_id])
     @section = Section.new
   end
 
   def create
-    @course = Course.find(params[:course_id])
-    @section = @course.sections.create(section_params)
-    redirect_to instructor_course_path(@course)
+    @section = current_course.sections.create(section_params)
+    redirect_to instructor_course_path(current_course)
   end
 
   private
