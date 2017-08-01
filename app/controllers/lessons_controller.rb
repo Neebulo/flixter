53b5076ffc494204f9f38cb3cec50dd14cc90a98
neebulo <rhapsodyshadow@yahoo.com>
2017-08-01 23:34:49 +0000

    added current_lesson helper method | student lessons_controller.rb

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -5,5 +5,19 @@ class LessonsController < ApplicationController
 
 
 
-  
+
+
+
+
+
+  private
+
+  helper_method :current_lesson
+  def current_lesson
+    @current_lesson ||= Lesson.find(params[:id])
+  end
+
+
+
+
 end
