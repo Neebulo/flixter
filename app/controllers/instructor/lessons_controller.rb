bcd6e3dceb2e064bd77bab3cfe93bdb8ab2408c5
neebulo <rhapsodyshadow@yahoo.com>
2017-08-01 05:12:19 +0000

    refactored before_action unauth message with def require_authorized_for_current_section

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,17 +1,12 @@
 class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_authorized_for_current_section
 
   def new
-    if current_section.course.user != current_user
-      return render plain: 'Unauthorized', status: :unauthorized
-    end
     @lesson = Lesson.new
   end
 
   def create
-    if current_section.course.user != current_user
-      return render plain: 'Unauthorized', status: :unauthorized
-    end
     @lesson = current_section.lessons.create(lesson_params)
     redirect_to instructor_course_path(current_section.course)
   end
