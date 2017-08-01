5f9d1c493aad285658da14e26b602a2268baf77c
neebulo <rhapsodyshadow@yahoo.com>
2017-08-01 07:24:24 +0000

    before_action for unauth message added | instructor courses_controller.rb

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,5 +1,6 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
 
   def new
     @course = Course.new
@@ -21,6 +22,12 @@ class Instructor::CoursesController < ApplicationController
 
 
   private
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
 
   helper_method :current_course
   def current_course
