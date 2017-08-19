bfb51499816394ef36f529f43e7a57b78d39a8e5
neebulo <rhapsodyshadow@yahoo.com>
2017-08-19 06:49:19 +0000

    refactor further of enrolled_in?

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   def enrolled_in?(course)
-    enrolled_courses = enrollments.collect do |enrollment|
-        enrollment.course
-    end
-
-  return enrolled_courses.include?(course)
+    enrolled_courses = enrollments.collect(&:course)
+    return enrolled_courses.include?(course)
+  end
 
 end
