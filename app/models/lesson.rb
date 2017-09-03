6e752881dd68e9d20c55c5ea514aa84539f6f06c
Cris <rhapsodyshadow@yahoo.com>
2017-09-03 00:42:15 -0700

    next_lesson code |lesson.rb

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -7,6 +7,8 @@ class Lesson < ApplicationRecord
   ranks :row_order, with_same: :section_id
 
   def next_lesson
+    lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
+    return lesson
   end
 
 end
